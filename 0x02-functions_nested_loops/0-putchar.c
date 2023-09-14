49518a68a78dc34f0d0a909b129548b6f4db7cc6
Romisa-nor <romusanor2001@gmail.com>
2023-09-14 09:05:53 -0700

    all in

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * main - Print _putchar
- * Return: Always 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
-=======
-#include <main.h>
-=======
-#include<main.h>
->>>>>>> c71e37ad3e2398b5ff43bff94006eeea86585d82
-
-/**
- * main - Print _putchar
- * Return: Always 0
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
->>>>>>> 8c21b72b0187eddcdef2f21dd7d5602b6ca9c4bd
-=======
 #include "main.h"
 
 /**
@@ -67,4 +20,4 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
->>>>>>> 8ce8c8770121a10494e34a48104ad6c10e37f57d
+
