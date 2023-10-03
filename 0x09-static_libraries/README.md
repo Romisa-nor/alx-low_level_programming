3ddd4263ad53e5a74a41bcfd2e7ca105d9fbf4fc
Romisa-nor <romusanor2001@gmail.com>
2023-10-03 02:12:24 -0700

    a

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+ I'm in 0x09-static_libraries 
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar take char and give int
+ *
+ * Return: int
+ */
+int _putchar(char c)
+{
+	return (write(i, &c, 1));
+}
