ebbf80e86acefcacd33dd617de042c38e46ff667
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-10-21 00:48:05 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,19 +7,19 @@
 
 /**
  * list_len - Finds the number of elements in
- * a linked list_t list.
- * @l: The linked list_t list.
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: The number of elements in l.
+ * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
 
-	while (l)
+	while (h)
 	{
 		elements++;
-		l = l->next;
+		h = h->next;
 	}
 
 	return (elements);
